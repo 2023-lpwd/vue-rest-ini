52ebd7476068a29793ffdf3619067860fb0f132a
mccp <parent.clement2@gmail.com>
2023-01-10 11:48:49 +0100

    [feature] - Add CategoryView requests

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -1,11 +1,55 @@
 <template>
   <main class="category-view">
-    category view
+    <div class="container">
+      <div class="products-list || row">
+        <div v-for="(product, index) in products" class="products-item || column -size-3">
+          <Product v-bind="product" />
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
-export default {}
+import Product from "@/components/Product.vue"
+import { client } from "@/utils/axios"
+
+export default {
+  components: {
+    Product
+  },
+
+  data () {
+    return {
+      products: []
+    }
+  },
+
+  async mounted () {
+    await this.getProducts()
+  },
+
+  // To switch between categories routes
+  async updated () {
+    await this.getProducts()
+  },
+
+  methods: {
+    async getProducts () {
+      // Get $route dynamic parameter
+      const routeParam = this.$route.params.category
+
+      // Get axios response to get current category
+      const categoryResponse = await client.get(import.meta.env.VITE_WP_API_URL + '/wc/v3/products/categories?slug=' + routeParam)
+      const [category] = categoryResponse.data
+
+      // Get axios response to get products with current category
+      const productsResponse = await client.get(import.meta.env.VITE_WP_API_URL + '/wc/v3/products?category=' + category.id)
+
+      this.products = productsResponse.data
+    }
+  }
+}
 </script>
 
 <style lang="scss">
