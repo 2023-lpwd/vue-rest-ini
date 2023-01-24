c661da79de5700fda909d47e986cde4153d2eb58
mccp <parent.clement2@gmail.com>
2023-01-24 16:06:24 +0100

    [template] - Add form fields to OrderView

--- src/views/OrderView.vue
+++ src/views/OrderView.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <h1 class="order-view__title">Commander</h1>
       <form action="" class="order-view__form">
-        <div class="row">
+        <div class="order-view__row || row">
           <div class="column -size-6">
             <div class="order-view__field">
               <label class="order-view__label" for="firstname">Prénom</label>
@@ -14,6 +14,62 @@
             <div class="order-view__field">
               <label class="order-view__label" for="lastname">Nom</label>
               <input class="order-view__input" id="lastname" type="text" name="last_name" v-model="form.billing.last_name">
+            </div>
+          </div>
+        </div>
+        <div class="order-view__row || row">
+          <div class="column -size-6">
+            <div class="order-view__field">
+              <label class="order-view__label" for="address">Adresse</label>
+              <input class="order-view__input" id="address" type="text" name="address_1" v-model="form.billing.address_1">
+            </div>
+          </div>
+          <div class="column -size-6">
+            <div class="order-view__field">
+              <label class="order-view__label" for="extra-address">Complément d'adresse</label>
+              <input class="order-view__input" id="extra-address" type="text" name="address_2" v-model="form.billing.address_2">
+            </div>
+          </div>
+        </div>
+        <div class="order-view__row || row">
+          <div class="column -size-6">
+            <div class="order-view__field">
+              <label class="order-view__label" for="city">Ville</label>
+              <input class="order-view__input" id="city" type="text" name="city" v-model="form.billing.city">
+            </div>
+          </div>
+          <div class="column -size-6">
+            <div class="order-view__field">
+              <label class="order-view__label" for="state">État/région</label>
+              <input class="order-view__input" id="state" type="text" name="state" v-model="form.billing.state">
+            </div>
+          </div>
+        </div>
+        <div class="order-view__row || row">
+          <div class="column -size-6">
+            <div class="order-view__field">
+              <label class="order-view__label" for="zipcode">Code postal</label>
+              <input class="order-view__input" id="zipcode" type="text" name="zipcode" v-model="form.billing.zipcode">
+            </div>
+          </div>
+          <div class="column -size-6">
+            <div class="order-view__field">
+              <label class="order-view__label" for="country">Pays</label>
+              <input class="order-view__input" id="country" type="text" name="country" v-model="form.billing.country">
+            </div>
+          </div>
+        </div>
+        <div class="order-view__row || row">
+          <div class="column -size-6">
+            <div class="order-view__field">
+              <label class="order-view__label" for="email">Email</label>
+              <input class="order-view__input" id="email" type="email" name="email" v-model="form.billing.email">
+            </div>
+          </div>
+          <div class="column -size-6">
+            <div class="order-view__field">
+              <label class="order-view__label" for="phone">Téléphone</label>
+              <input class="order-view__input" id="phone" type="text" name="phone" v-model="form.billing.phone">
             </div>
           </div>
         </div>
